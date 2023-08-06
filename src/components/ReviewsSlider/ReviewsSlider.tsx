b33109f49b78738e7c0ca34c6b69d397bfe3c961
bryzZz <nikita.tebloev@polyus-nt.ru>
2023-08-06 15:20:40 +0900

    add real testimonials

--- src/components/ReviewsSlider/ReviewsSlider.tsx
+++ src/components/ReviewsSlider/ReviewsSlider.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 import React, { useRef } from "react";
 import { Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -7,10 +8,12 @@ import { ReactComponent as Star } from "assets/icons/star.svg";
 
 import "swiper/css";
 import "./style.css";
+import { twMerge } from "tailwind-merge";
 
 interface ReviewsSliderProps {
   items: {
     avatar: string;
+    avatarBg?: boolean;
     title: string;
     date: string;
     text: string;
@@ -22,6 +25,12 @@ interface ReviewsSliderProps {
 export const ReviewsSlider: React.FC<ReviewsSliderProps> = ({ items }) => {
   const prevRef = useRef<HTMLButtonElement>(null);
   const nextRef = useRef<HTMLButtonElement>(null);
+
+  const getSiteName = (link: string) => {
+    return link.match(
+      /^(https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n?]+)/
+    )?.[2];
+  };
 
   return (
     <div className="flex items-center gap-10 lg:gap-20 xl:gap-[114px]">
@@ -51,52 +60,63 @@ export const ReviewsSlider: React.FC<ReviewsSliderProps> = ({ items }) => {
           },
         }}
       >
-        {items.map(({ avatar, date, title, text, source, rating }, i) => (
-          <SwiperSlide
-            className="w-full max-w-[306px] select-none md:max-w-sm lg:max-w-md xl:max-w-lg"
-            key={i}
-          >
-            <div className="mt-3 rounded-2xl bg-text-100 px-[22px] pb-5 shadow-100">
-              <div className="flex items-center gap-4">
-                <img
-                  className="aspect-square w-[70px] -translate-y-3 3xl:w-[95px]"
-                  src={avatar}
-                />
-                <div className="flex-1">
-                  <div className="flex items-center justify-between">
-                    <p className="text-[13px] font-semibold leading-[10px] text-text-200 3xl:text-lg">
-                      {date}
-                    </p>
-                    {rating && (
-                      <p className="flex items-center gap-[2px] text-[10px] font-bold text-[#C8C8C8] 3xl:text-sm">
-                        <Star />
-                        <span className="text-[13px] text-[#309E3A] 3xl:text-base">
-                          {rating}
-                        </span>
-                        /10
-                      </p>
+        {items.map(
+          (
+            { avatar, avatarBg = false, date, title, text, source, rating },
+            i
+          ) => (
+            <SwiperSlide
+              className="h-[unset] w-full max-w-[306px] select-none md:max-w-sm lg:max-w-md xl:max-w-lg"
+              key={i}
+            >
+              <div className="mt-3 flex h-[calc(100%-20px)] w-full flex-col rounded-2xl bg-text-100 px-[22px] pb-5 shadow-100">
+                <div className="flex items-center gap-4">
+                  <div
+                    className={twMerge(
+                      "aspect-square w-[70px] -translate-y-3 rounded-md 3xl:w-[95px]",
+                      avatarBg && "bg-[#e5e5e5] p-2"
                     )}
-                  </div>
-                  <h6 className="text-[15px] font-bold text-text-400 3xl:text-xl">
-                    {title}
-                  </h6>
-                </div>
-              </div>
-              <p className="mb-3 text-sm text-text-300 3xl:text-lg">{text}</p>
-              {source && (
-                <p className="text-right text-[13px] font-semibold text-[#C0C0C0] 3xl:text-base">
-                  Отзыв с сайта:{" "}
-                  <a
-                    href={source}
-                    className="transition-colors hover:text-[#3452FF]"
                   >
-                    {source}
-                  </a>
-                </p>
-              )}
-            </div>
-          </SwiperSlide>
-        ))}
+                    <img className="h-full w-full" src={avatar} />
+                  </div>
+                  <div className="flex-1">
+                    <div className="flex items-center justify-between">
+                      <p className="text-[13px] font-semibold leading-[10px] text-text-200 3xl:text-lg">
+                        {date}
+                      </p>
+                      {rating && (
+                        <p className="flex items-center gap-[2px] text-[10px] font-bold text-[#C8C8C8] 3xl:text-sm">
+                          <Star />
+                          <span className="text-[13px] text-[#309E3A] 3xl:text-base">
+                            {rating}
+                          </span>
+                          /10
+                        </p>
+                      )}
+                    </div>
+                    <h6 className="text-[15px] font-bold text-text-400 3xl:text-xl">
+                      {title}
+                    </h6>
+                  </div>
+                </div>
+
+                <p className="mb-3 text-sm text-text-300 3xl:text-lg">{text}</p>
+
+                {source && (
+                  <p className="mt-auto text-right text-[13px] font-semibold text-[#C0C0C0] 3xl:text-base">
+                    Отзыв с сайта:{" "}
+                    <a
+                      href={source}
+                      className="transition-colors hover:text-[#3452FF]"
+                    >
+                      {getSiteName(source)}
+                    </a>
+                  </p>
+                )}
+              </div>
+            </SwiperSlide>
+          )
+        )}
       </Swiper>
     </div>
   );
