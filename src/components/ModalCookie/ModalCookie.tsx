4a361ebcc47e041a8dda9f73d1f8dc9832be8e03
bryzZz <nikita.tebloev@polyus-nt.ru>
2023-08-30 19:31:21 +0800

    fixes

--- src/components/ModalCookie/ModalCookie.tsx
+++ src/components/ModalCookie/ModalCookie.tsx
@@ -1,41 +1,60 @@
 import React, { useEffect, useState } from "react";
 import Modal from "react-modal";
 
-import useLocalStorage from "hooks/useLocalStorage";
-
 import "./style.css";
 
 Modal.setAppElement("#root");
 
+// Бред какой-то
 export const ModalCookie: React.FC = () => {
-  const [lastCookieDate, setLastCookieDate] = useLocalStorage(
-    "leadshub-cookie",
-    { date: new Date() }
-  );
+  const [lastDate, setLastDate] = useState(() => {
+    try {
+      return localStorage.getItem("leadshub-cookie");
+    } catch (error) {
+      return null;
+    }
+  });
+  const [firstSetted, setFirstSetted] = useState(false);
+
   const [isOpen, setIsOpen] = useState(false);
+  const open = () => setIsOpen(true);
+  const close = () => setIsOpen(false);
 
   useEffect(() => {
     const now = new Date();
 
+    if (lastDate === null) {
+      open();
+      setFirstSetted(true);
+      setLastDate(now.toString());
+      return;
+    }
+
+    if (firstSetted) return;
+
     const t2 = now.getTime();
-    const t1 = new Date(lastCookieDate.date).getTime();
+    const t1 = new Date(lastDate).getTime();
 
     const diff = Math.floor((t2 - t1) / (24 * 3600 * 1000));
 
     if (diff < 0 || diff > 30) {
-      setLastCookieDate({ date: now });
-      setIsOpen(true);
-    }
-  }, [lastCookieDate, setLastCookieDate]);
+      open();
 
-  const onClose = () => {
-    setIsOpen(false);
-  };
+      setLastDate(now.toString());
+      // localStorage.setItem("leadshub-cookie", now.toString());
+    }
+  }, [firstSetted, lastDate]);
+
+  useEffect(() => {
+    if (lastDate) {
+      localStorage.setItem("leadshub-cookie", lastDate);
+    }
+  }, [lastDate]);
 
   return (
     <Modal
       isOpen={isOpen}
-      onRequestClose={onClose}
+      onRequestClose={close}
       overlayClassName="ModalCookie__overlay"
       className="ModalCookie"
     >
@@ -51,13 +70,13 @@ export const ModalCookie: React.FC = () => {
         <div className="flex items-center justify-center gap-3">
           <button
             className="btn-secondary px-5 py-2 text-xs font-bold 3xl:px-6 3xl:py-4 3xl:text-lg"
-            onClick={onClose}
+            onClick={close}
           >
             Принять
           </button>
           <button
             className="btn-ternary px-5 py-2 text-xs font-bold 3xl:px-6 3xl:py-4 3xl:text-lg"
-            onClick={onClose}
+            onClick={close}
           >
             Запретить
           </button>
